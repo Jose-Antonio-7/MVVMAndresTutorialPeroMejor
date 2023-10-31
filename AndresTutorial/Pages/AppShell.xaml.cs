86967d36ff377371487a22e5ccd17d91186e8ca5
Antonio Rivera <antonio.rivera@techsoft.com.mx>
2023-10-31 13:25:02 -0700

    Se agrego diseño de pantalla bienvenida

--- AndresTutorial/Pages/AppShell.xaml.cs
+++ AndresTutorial/Pages/AppShell.xaml.cs
@@ -5,8 +5,8 @@ public partial class AppShell : Shell
 	public AppShell()
 	{
 		InitializeComponent();
-		Routing.RegisterRoute(nameof(BienvenidaPage),typeof(BienvenidaPage));
-        Routing.RegisterRoute(nameof(Login), typeof(Login));
+		//Routing.RegisterRoute(nameof(BienvenidaPage),typeof(BienvenidaPage));
+        //Routing.RegisterRoute(nameof(Login), typeof(Login));
         //Routing.RegisterRoute(nameof(ListadoPedidosPage), typeof(ListadoPedidosPage));
 
 
